48b8860886b946db49d95db7fccb3077ee2987b6
abuzainn <abuzainn@mcmaster.ca>
2026-03-06 15:10:27 -0500

    Update 2dx3_deliverable1.c
    completed initializing ports for deliverable 1

--- 2dx3_deliverable1.c
+++ 2dx3_deliverable1.c
@@ -1,9 +1,5 @@
-// COMPENG 2DX3
-// This program illustrates the interfacing of the Stepper Motor with the microcontroller
-
-//  Written by Ama Simons
-//  January 18, 2020
-// 	Last Update by Dr. Shahrukh Athar on February 2, 2025
+//COMPENG 2DX3
+//PROJECT DELIVERABLE 1
 
 #include <stdint.h>
 #include "tm4c1294ncpdt.h"
@@ -17,8 +13,6 @@ void PortJ_Init(void) //Port J is for the two onboard user switches (what does t
   while((SYSCTL_PRGPIO_R & 0x08) == 0){};
   GPIO_PORTJ_DIR_R &= 0xFC; // enabling pins 0 and 1 as input
   GPIO_PORTJ_DEN_R |= 0x03; // enabling pins 0 and 1 as GPIO
-  GPIO_PORTJ_AFSEL_R &= ~0x03; // disable alt function
-  GPIO_PORTJ_AMSEL_R &= ~0x03; // disabling analog functionality
 }
 
 void PortM_Init(void) //Port M is for implemented buttons 
@@ -27,11 +21,29 @@ void PortM_Init(void) //Port M is for implemented buttons
   while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R11) == 0){}; // wait for the clock to stabilize
   GPIO_PORTM_DEN_R |= 0x03; //enabling pins 0 and 1
   GPIO_PORTM_DIR_R &= 0xFC; //making pins as input
-  GPIO_PORTM_AFSEL_R &= ~0x03;
-  GPIO_PORTM_AMSEL_R &= ~0x03;
 }
 
-void PortH_Init(void)
+void PortH_Init(void) //Port H is for controlling motor
 {
-  
+  SYSCTL_RCGCPIO_R |= 0x07; //enable clock
+  while((SYSCTL_PRGPIO_R & 0x07) == 0){}; 
+  GPIO_PORTH_DEN_R |= 0x0F; //enabling pins 0 to 4
+  GPIO_PORTH_DIR_R |= 0x0F; //setting pins as output
 }
+
+void PortN_Init(void) //Controls 2 LEDs
+{
+  SYSCTL_RCGCPIO_R |= 0x0C; //enable clock
+  while((SYSCTL_PRGPIO_R & 0x0C) == 0){};
+  GPIO_PORTN_DEN_R |= 0x03; //setting up pins 0 and 1
+  GPIO_PORTN_DIR_R |= 0x03; 
+}
+
+void PortF_Init(void) //controls 2 other LEDS
+{
+  SYSCTL_RCGCPIO_R |= 0x05;
+  while((SYSCTL_PRGPIO_R & 0x05) == 0){};
+  GPIO_PORTN_DEN_R |= 0x03;
+  GPIO_PORTN_DIR_R |= 0x03;
+}
+
