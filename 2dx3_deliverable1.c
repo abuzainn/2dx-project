4bb46b9f519ee5ba673cde541886b1c3a114c5fe
abuzainn <abuzainn@mcmaster.ca>
2026-03-07 09:37:55 -0500

    Update 2dx3_deliverable1.c
    further work done, fixed some ports

--- 2dx3_deliverable1.c
+++ 2dx3_deliverable1.c
@@ -9,7 +9,7 @@
 //Setting up ints - WE CANNOT USE BOOL
 int MotorRunning = 0;
 int CW = 1;
-bool FullStep = 1;
+int FullStep = 0;
 
 void PortJ_Init(void) //Port J is for the two onboard user switches (what does this mean?)
 {
@@ -17,6 +17,7 @@ void PortJ_Init(void) //Port J is for the two onboard user switches (what does t
   SYSCTL_RCGCGPIO_R |= 0x08; //enable clock
   while((SYSCTL_PRGPIO_R & 0x08) == 0){};
   GPIO_PORTJ_DIR_R &= 0xFC; // enabling pins 0 and 1 as input
+  GPIO_PORTJ_PCTL_R &= ~0x000000F0;
   GPIO_PORTJ_DEN_R |= 0x03; // enabling pins 0 and 1 as GPIO
   GPIO_PORTJ_PDR_R |= 0x03; // enabling internal pulldown
   return;
@@ -24,7 +25,7 @@ void PortJ_Init(void) //Port J is for the two onboard user switches (what does t
 
 void PortM_Init(void) //Port M is for implemented buttons 
 {
-  SYSCTL_RCGCPIO_R |= SYSCTL_RCGCPIO_R11; //enable clock
+  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R11; //enable clock
   while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R11) == 0){}; // wait for the clock to stabilize
   GPIO_PORTM_DEN_R |= 0x03; //enabling pins 0 and 1
   GPIO_PORTM_DIR_R &= 0xFC; //making pins as input
@@ -34,7 +35,7 @@ void PortM_Init(void) //Port M is for implemented buttons
 
 void PortH_Init(void) //Port H is for controlling motor. 
 {
-  SYSCTL_RCGCPIO_R |= 0x07; //enable clock
+  SYSCTL_RCGCGPIO_R |= 0x07; //enable clock
   while((SYSCTL_PRGPIO_R & 0x07) == 0){}; 
   GPIO_PORTH_DEN_R |= 0x0F; //enabling pins 0 to 4
   GPIO_PORTH_DIR_R |= 0x0F; //setting pins as output
@@ -43,7 +44,7 @@ void PortH_Init(void) //Port H is for controlling motor.
 
 void PortN_Init(void) //Controls D1 and D2 LEDs
 {
-  SYSCTL_RCGCPIO_R |= 0x0C; //enable clock
+  SYSCTL_RCGCGPIO_R |= 0x0C; //enable clock
   while((SYSCTL_PRGPIO_R & 0x0C) == 0){};
   GPIO_PORTN_DEN_R |= 0x03; //setting up pins 0 and 1
   GPIO_PORTN_DIR_R |= 0x03; 
@@ -52,66 +53,134 @@ void PortN_Init(void) //Controls D1 and D2 LEDs
 
 void PortF_Init(void) //Controls D3 and D4 LEDs
 {
-  SYSCTL_RCGCPIO_R |= 0x05;
+  SYSCTL_RCGCGPIO_R |= 0x05;
   while((SYSCTL_PRGPIO_R & 0x05) == 0){};
-  GPIO_PORTN_DEN_R |= 0x03;
-  GPIO_PORTN_DIR_R |= 0x03;
+  GPIO_PORTF_DEN_R |= 0x11;
+  GPIO_PORTF_DIR_R |= 0x11;
   return;
 }
 
 //BUTTON READING FUNCTIONS, going to use pull up resistor
 
-bool Button0Pressed() //controlled by PJ0
+int Button0Pressed() //controlled by PJ0
 {
-  return (GPIO_PORTJ_DATA_R & 0x01) == 1;
+  return GPIO_PORTJ_DATA_R & 0x01;
 }
 
-bool Button1Pressed() //controlled by PJ1
+int Button1Pressed() //controlled by PJ1
 {
-  return (GPIO_PORTJ_DATA_R & 0x02) == 1;
+  return GPIO_PORTJ_DATA_R & 0x02;
 }
 
-bool Button2Pressed() //controlled by PM0
+int Button2Pressed() //controlled by PM0
 {
-  return (GPIO_PORTM_DATA_R & 0x01) == 1;
+  return GPIO_PORTM_DATA_R & 0x01;
 }
 
-bool Button3Pressed()
+int Button3Pressed()
 {
-  return (GPIO_PORTM_DATA_R & 0x02) == 1;
+  return GPIO_PORTM_DATA_R & 0x02;
 }
 
 void FullStepSpinCW() 
 {
 	uint32_t delay = 1;	
-  while(1)
+	int stepCount = 0;
+  for(int i = 0; i < 512 && Button0Pressed() == 0; i++)
     {
-		GPIO_PORTM_DATA_R = 0b00000011;
+		GPIO_PORTH_DATA_R = 0b00000011;
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00000110;			
+		GPIO_PORTH_DATA_R = 0b00000110;			
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00001100;	
+		GPIO_PORTH_DATA_R = 0b00001100;	
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00001001;
+		GPIO_PORTH_DATA_R = 0b00001001;
 		SysTick_Wait10ms(delay);
+		stepCount++;
+		if(stepCount % 64 == 0) //toggling light every 45 deg
+		{
+			GPIO_PORTF_DATA_R ^= 0x01;
+			SysTick_Wait10ms(2);
+			GPIO_PORTF_DATA_R ^= 0x01;
+		}
     }
+	MotorRunning = 0;
 }
 
 void FullStepSpinCCW() 
 {
-	uint32_t delay = 1;	
-  while(1)
+	uint32_t delay = 1;
+	int stepCount = 0;
+  for(int i = 0; i < 512 && Button0Pressed() == 0; i++)
     {
-		GPIO_PORTM_DATA_R = 0b00001001; //0b00001001
+		GPIO_PORTH_DATA_R = 0b00001001; //0b00001001
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00001100;	//0b00001100	
+		GPIO_PORTH_DATA_R = 0b00001100;	//0b00001100	
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00000110;	//0b00000110
+		GPIO_PORTH_DATA_R = 0b00000110;	//0b00000110
 		SysTick_Wait10ms(delay);
-		GPIO_PORTM_DATA_R = 0b00000011; //0b00000011
+		GPIO_PORTH_DATA_R = 0b00000011; //0b00000011
 		SysTick_Wait10ms(delay);
+		stepCount++;
+		if(stepCount % 64 == 0)
+		{
+			GPIO_PORTF_DATA_R ^= 0x01;
+			SysTick_Wait10ms(2);
+			GPIO_PORTF_DATA_R ^= 0x01;
+		}
     
     }
+	MotorRunning = 0; //motor should stop after this function is done
+}
+
+void HalfStepSpinCW()
+{
+	uint32_t delay = 1;
+	int stepCount = 0;
+	for(int i = 0; i < 512 && Button0Pressed() == 0; i++)
+		{
+			GPIO_PORTH_DATA_R = 0b00000001;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00000010;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00000100;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00001000;
+			SysTick_Wait10ms(delay);
+			stepCount++;
+			if(stepCount % 16 == 0)
+			{
+				GPIO_PORTF_DATA_R ^= 0x01;
+				SysTick_Wait10ms(2);
+				GPIO_PORTF_DATA_R ^= 0x01;
+			}
+		}
+	MotorRunning = 0;
+}
+
+void HalfStepSpinCCW()
+{
+	uint32_t delay = 1;
+	int stepCount = 0;
+	for(int i = 0; i < 512 && Button0Pressed() == 0; i++)
+		{
+			GPIO_PORTH_DATA_R = 0b00001000;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00000100;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00000010;
+			SysTick_Wait10ms(delay);
+			GPIO_PORTH_DATA_R = 0b00000001;
+			SysTick_Wait10ms(delay);
+			stepCount++;
+			if(stepCount % 16 == 0) //blinking light every 11.25 deg
+			{
+				GPIO_PORTF_DATA_R ^= 0x01;
+				SysTick_Wait10ms(2);
+				GPIO_PORTF_DATA_R ^= 0x01;
+			}	
+		}
+	MotorRunning = 0;
 }
 
 
@@ -160,12 +229,12 @@ int main(void)
       if(FullStep == 1)
       {
         FullStep = 0; 
-        //something with the lights
+        GPIO_PORTF_DATA_R |= 0x10; //setting PF4 to 1
       }
       else
       {
         FullStep = 1;
-        //something with the light
+        GPIO_PORTF_DATA_R &= ~0x10; //turning off the led
       }
     }
     if(Button3Pressed())
